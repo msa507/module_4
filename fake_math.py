9e592610ecf188c4f6b76e47cc9da601c4b2440a
msa507 <msa10101962@gmail.com>
2024-11-14 10:36:15 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide (first, second):
+    if second == 0:
+        print('Ошибка')
+    else:
+        div = first / second
+        print(div)
+    return ()
+
---
+++ true_math.py
@@ -0,0 +1,10 @@
+import math
+
+def divide (first, second):
+    if second == 0:
+        print(math.inf)
+    else:
+        div = first / second
+        print(div)
+    return ()
+
